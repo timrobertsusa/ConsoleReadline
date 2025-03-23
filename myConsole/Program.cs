7c68eab819eec6a8639fd906feff7c6273fbf999
Tim Roberts <timrobertsusa@pluralpoint.com>
2025-03-23 10:03:09 -0700

    first commit

---
+++ myConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+while (true)
+{
+    Console.WriteLine("Enter input (type 'exit' to quit):");
+    string? input = Console.ReadLine();
+    if (input?.ToLower() == "exit")
+    {
+        break;
+    }
+    Console.WriteLine($"You entered: {input}");
+}
